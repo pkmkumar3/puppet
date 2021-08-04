e8c6396c364941eb485901dbb9c2948f1cccea03
pkmkumar3 <53393741+pkmkumar3@users.noreply.github.com>
2021-08-04 16:28:35 +0530

    Create ntp.pp

---
+++ ntp.pp
@@ -0,0 +1,26 @@
+case $facts ['os'] ['family'] ['full'] {
+  'RedHat', '6':
+     package {'ntp':
+       name   => 'ntp',
+       ensure => installed,
+       }
+     file {'/etc/sysconfig/ntpd':
+       ensure  => present
+       content => 'OPTIONS="-u ntpd"',
+     }
+     service {'ntpd'
+      ensure => running,
+      enable => true,
+  'RedHat', '7':
+     package {'chrony':
+       name   => 'chrony',
+       ensure => installed,
+     file {'/etc/sysconfig/chronyd':,
+       ensure  => present,
+       content => 'OPTIONS="-u chrony"',
+       }
+     service {'chronyd'
+      ensure => running,
+      enable => true, 
+      }
+       
