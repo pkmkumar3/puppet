648f91220e545d04ee86be61c88d46ea6642bf28
pkmkumar3 <53393741+pkmkumar3@users.noreply.github.com>
2021-08-04 15:17:23 +0530

    Create mainifest1.pp

---
+++ mainifest1.pp
@@ -0,0 +1,48 @@
+class apache {
+
+package {'httpd package':
+  name   => httpd,
+  ensure => installed,
+  
+} 
+
+ $http.conf = '<IfModule prefork.c>
+               StartServers        4
+               MinSpareServers     20
+               MaxSpareServers     40
+               MaxClients          200
+               MaxRequestsPerChild 4500
+               </IfModule>'
+file { '/etc/httpd/conf/httpd.conf':
+  ensure  => present,
+  content => $http.conf,
+}
+
+$vhost = '<VirtualHost *:80>
+             ServerAdmin admin@example.org
+             ServerName example.org
+             ServerAlias www.example.org
+             DocumentRoot /srv/www/example.org/public_html/
+             ErrorLog /srv/www/example.org/logs/error.log
+             CustomLog /srv/www/example.org/logs/access.log combined
+        </VirtualHost>'
+
+file {'/etc/httpd/conf.d/vhost.conf':
+  ensure  => present,
+  content => $vhost,
+}
+
+file {'/srv/www/example.org/public_html':
+  ensure  => directory,
+  recurse => true,
+}
+
+file {'/srv/www/example.org/logs':
+  ensure  => directory,
+  recurse => true,
+}
+
+service {'httpd':
+  ensure => running,
+  enable => true,
+}
